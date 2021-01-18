818734b4a730e7ba94bf1d302b48d1d3b954be55
Nathan Rothenberger <nroth10@gmail.com>
2021-01-17 18:46:05 -0600

    theme updates

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -3,6 +3,7 @@
 const defaultFontStack = `Helvetica, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`
 
 export default {
+  breakpoints: ["48em", "64em"], // 640 // 896 // 1024
   // defaults
   colors: {
     text: "#111",
@@ -11,7 +12,7 @@ export default {
     primary: "blue",
     secondary: "green",
   },
-  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
+  // fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
   fonts: {
     body: defaultFontStack,
     heading: defaultFontStack,
@@ -29,21 +30,36 @@ export default {
   },
   sizes: {
     container: 1200,
+    full: "100%",
+    vh: "100vh",
+    vw: "100vw",
+  },
+  layout: {
+    container: {
+      width: "vw",
+      minHeight: 100,
+      px: 24,
+      bg: ["primary", "secondary", "debug"],
+    },
   },
   styles: {
     root: {
       fontFamily: "body",
       fontWeight: "body",
+      bg: ["red", "orange", "yellow"],
     },
     h1: {
       variant: "text.heading",
-      fontSize: 48,
-      color: "secondary",
+      // fontSize: 6,
+      fontSize: [32, 48],
+      color: "text",
+      m: 0,
     },
     h2: {
       variant: "text.heading",
       fontSize: 24,
       color: "hotpink",
+      m: 0,
     },
   },
 }
