4e121a66c09180750e073d865e8eb187e0f91e19
Nathan Rothenberger <nroth10@gmail.com>
2021-01-17 19:33:44 -0600

    cleanup footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,45 +1,30 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui"
+import { jsx, Flex, Styled } from "theme-ui"
 import React from "react"
 
 const Footer = () => {
   return (
-    <footer
+    <Flex
+      as="footer"
       sx={{
         mt: "24px",
         mb: "48px",
-        display: "flex",
         justifyContent: "space-between",
         alignItems: "flex-end",
-        fontFamily: "Helvetica",
-        fontWeight: "bold",
-        color: "text",
         a: {
           color: "inherit",
         },
       }}
     >
-      <h2
-        sx={{
-          m: "0",
-          lineHeight: "1",
-          fontSize: "24px",
-        }}
-      >
-        RTTNBRGR
-      </h2>
-      <div
-        sx={{
-          fontSize: "16px",
-        }}
-      >
+      <Styled.h2>RTTNBRGR</Styled.h2>
+      <Styled.p sx={{ fontWeight: "bold" }}>
         ©{new Date().getFullYear()} {` · `} Built with
         {` `}
         <a href="https://www.gatsbyjs.com">Gatsby</a>
         {` and `}
         <a href="https://theme-ui.com/">Theme UI</a>
-      </div>
-    </footer>
+      </Styled.p>
+    </Flex>
   )
 }
 
