5947ca857cc15cb6be6c7a42256abc61611172e2
Nathan Rothenberger <nroth10@gmail.com>
2021-01-17 18:42:40 -0600

    stub out portfolio data

---
+++ src/data/portfolio.js
@@ -0,0 +1,88 @@
+export default [
+  {
+    id: 1,
+    name: "Display",
+    client: "Sprinklr",
+  },
+  {
+    id: 2,
+    name: "Space Design System",
+    client: "Sprinklr",
+  },
+  {
+    id: 3,
+    name: "Modern Content Marketing",
+    client: "Sprinklr",
+  },
+  {
+    id: 4,
+    name: "WHO Covid Dashboard",
+    client: "Sprinklr",
+  },
+  {
+    id: 5,
+    name: "Logomarks",
+    client: "Misc",
+  },
+  {
+    id: 6,
+    name: "PSU Icons",
+    client: "Sprinklr",
+  },
+  {
+    id: 7,
+    name: "Jaguar",
+    client: "Aarongreq",
+  },
+  {
+    id: 8,
+    name: "ACQ Tunes",
+    client: "Acquired Taste Club",
+  },
+  {
+    id: 9,
+    name: "Redline Training",
+    client: "Redline Training",
+  },
+  {
+    id: 10,
+    name: "C-West Office Supplies",
+    client: "C-West Office Supplies",
+  },
+  {
+    id: 11,
+    name: "The Rarity",
+    client: "Acquired Taste Club",
+  },
+  {
+    id: 12,
+    name: "Mango",
+    client: "Personal",
+  },
+  {
+    id: 13,
+    name: "Aaronreq",
+    client: "Acquired Taste Club",
+  },
+  {
+    id: 14,
+    name: "Twilight Relayx XVI",
+    client: "Twilight Relays",
+  },
+
+  {
+    id: 15,
+    name: "Fullscreen Video",
+    client: "Personal",
+  },
+  {
+    id: 16,
+    name: "BroAm Poster",
+    client: "Redtruck",
+  },
+  {
+    id: 17,
+    name: "Who Are We Really",
+    client: "Acquired Taste Club",
+  },
+]
