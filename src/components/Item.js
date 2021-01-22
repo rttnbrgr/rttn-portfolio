81e01dafab7aee3141ed5bc89db01c5fb5ddccab
Nathan Rothenberger <nroth10@gmail.com>
2021-01-22 11:46:22 -0600

    make portfolio items link to sample project page

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,17 +1,18 @@
 /** @jsx jsx */
 import { jsx, Styled } from "theme-ui"
 import PropTypes from "prop-types"
+import { Link } from "gatsby"
 
-const Item = ({ title }) => {
+const Item = ({ title, onClick }) => {
   return (
-    <div>
+    <Link to="/sampleProject">
       <img
         src="https://picsum.photos/800/600"
         sx={{ mb: 12 }}
         alt="Meaniful Text"
       />
       <Styled.h2>{title}</Styled.h2>
-    </div>
+    </Link>
   )
 }
 
