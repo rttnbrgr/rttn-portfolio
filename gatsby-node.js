0a187fe5289f18f9aaec590e66fdeb959f0cc430
Nathan Rothenberger <nroth10@gmail.com>
2021-01-23 20:17:02 -0600

    use new template to build portfolio pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -41,7 +41,7 @@ exports.createPages = async ({ graphql, actions }) => {
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
-      component: path.resolve(`./src/pages/sampleProject.js`),
+      component: path.resolve(`./src/templates/portfolio-item.jsx`),
       context: {
         // Data passed to context is available
         // in page queries as GraphQL variables.
