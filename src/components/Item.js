cff8755522ffcb34a9a58e3a3853cd38159c5458
Nathan Rothenberger <nroth10@gmail.com>
2021-01-27 10:40:29 -0600

    add thumb support for the portfolio grid item

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,15 +2,24 @@
 import { jsx, Styled } from "theme-ui"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import Img from "gatsby-image"
 
-const Item = ({ title, to, onClick }) => {
+const Item = ({ title, to, onClick, thumb }) => {
   return (
     <Link to={to}>
-      <img
-        src="https://picsum.photos/800/600"
-        sx={{ mb: 12 }}
-        alt="Meaniful Text"
-      />
+      {thumb ? (
+        <Img
+          fluid={thumb.childImageSharp.fluid}
+          sx={{ mb: 12 }}
+          alt="Meaniful Text"
+        />
+      ) : (
+        <img
+          src="https://picsum.photos/800/600"
+          sx={{ mb: 12 }}
+          alt="Meaniful Text"
+        />
+      )}
       <Styled.h2>{title}</Styled.h2>
     </Link>
   )
@@ -19,6 +28,7 @@ const Item = ({ title, to, onClick }) => {
 Item.propTypes = {
   title: PropTypes.string.isRequired,
   to: PropTypes.string.isRequired,
+  thumb: PropTypes.any,
 }
 
 export default Item
