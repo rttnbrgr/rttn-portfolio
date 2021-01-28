510a81eee8b69816b559f20cc176eb5563ec1b61
Nathan Rothenberger <nroth10@gmail.com>
2021-01-27 22:17:19 -0600

    fix

--- src/templates/portfolio-item.jsx
+++ src/templates/portfolio-item.jsx
@@ -6,6 +6,7 @@ import Layout from "../components/layout"
 
 export default function PortfolioItem({ data }) {
   const post = data.markdownRemark
+  const { client, year, title, thumb } = post.frontmatter
   return (
     <Layout>
       <img
@@ -19,42 +20,43 @@ export default function PortfolioItem({ data }) {
           sx={{ mb: 12 }}
           alt="Meaniful Text"
         />
-      <Flex sx={{ overflow: "scroll", mb: 24 }}>
-        {[0, 1, 2, 3, 4].map(x => (
-          <img
-            src="https://picsum.photos/80/60"
-            alt="Meaniful Text"
-            key={x}
-            sx={{
-              ml: x === 0 ? 0 : 8,
-            }}
-          />
-        ))}
-      </Flex>
-      <Styled.h2 sx={{ lineHeight: 1 }}>{title}</Styled.h2>
-      {/* HR */}
-      <div
-        sx={{
-          height: "3px",
-          width: "24px",
-          bg: "text",
-          mb: 24,
-          mt: 16,
-        }}
-      />
-      <Flex sx={{ mb: 16 }}>
-        <div sx={{ flex: "0 0 50%" }}>
-          <Styled.h3 sx={{ lineHeight: 1, mb: 8 }}>Client</Styled.h3>
-          <Styled.p sx={{ lineHeight: 1 }}>{client}</Styled.p>
-        </div>
-        <div sx={{ flex: "0 0 50%" }}>
-          <Styled.h3 sx={{ lineHeight: 1, mb: 8 }}>Date</Styled.h3>
-          <Styled.p sx={{ lineHeight: 1 }}>{year}</Styled.p>
-        </div>
-      </Flex>
+        <Flex sx={{ overflow: "scroll", mb: 24 }}>
+          {[0, 1, 2, 3, 4].map(x => (
+            <img
+              src="https://picsum.photos/80/60"
+              alt="Meaniful Text"
+              key={x}
+              sx={{
+                ml: x === 0 ? 0 : 8,
+              }}
+            />
+          ))}
+        </Flex>
+        <Styled.h2 sx={{ lineHeight: 1 }}>{title}</Styled.h2>
+        {/* HR */}
+        <div
+          sx={{
+            height: "3px",
+            width: "24px",
+            bg: "text",
+            mb: 24,
+            mt: 16,
+          }}
+        />
+        <Flex sx={{ mb: 16 }}>
+          <div sx={{ flex: "0 0 50%" }}>
+            <Styled.h3 sx={{ lineHeight: 1, mb: 8 }}>Client</Styled.h3>
+            <Styled.p sx={{ lineHeight: 1 }}>{client}</Styled.p>
+          </div>
+          <div sx={{ flex: "0 0 50%" }}>
+            <Styled.h3 sx={{ lineHeight: 1, mb: 8 }}>Date</Styled.h3>
+            <Styled.p sx={{ lineHeight: 1 }}>{year}</Styled.p>
+          </div>
+        </Flex>
 
-      <Styled.h3 sx={{ lineHeight: 1, mb: 8 }}>Description</Styled.h3>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <Styled.h3 sx={{ lineHeight: 1, mb: 8 }}>Description</Styled.h3>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </Flex>
     </Layout>
   )
 }
